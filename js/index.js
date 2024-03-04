b72286e02c3646292425c2c800b35e45da5f71fb
김진호 <rlawlsgh1227@gmail.com>
2024-03-04 17:18:48 +0900

    feat: 인생 영화 best 5 기능 추가

---
+++ js/index.js
@@ -0,0 +1,30 @@
+const MOVIE_INFO = [
+  {
+    name: '베테랑',
+    link: 'https://search.naver.com/search.naver?where=nexearch&sm=tab_etc&mra=bkEw&pkid=68&os=1848831&qvt=0&query=%EB%B2%A0%ED%85%8C%EB%9E%91',
+  },
+  {
+    name: '극한직업',
+    link: 'https://search.naver.com/search.naver?where=nexearch&sm=tab_etc&mra=bkEw&pkid=68&os=6621719&qvt=0&query=%EA%B7%B9%ED%95%9C%EC%A7%81%EC%97%85',
+  },
+  {
+    name: '악마는 프라다를 입는다',
+    link: 'https://search.naver.com/search.naver?where=nexearch&sm=tab_etc&mra=bkEw&pkid=68&os=1794620&qvt=0&query=%EC%95%85%EB%A7%88%EB%8A%94%20%ED%94%84%EB%9D%BC%EB%8B%A4%EB%A5%BC%20%EC%9E%85%EB%8A%94%EB%8B%A4',
+  },
+  {
+    name: '엘리멘탈',
+    link: 'https://search.naver.com/search.naver?where=nexearch&sm=tab_etc&mra=bkEw&pkid=68&os=29284390&qvt=0&query=%EC%97%98%EB%A6%AC%EB%A9%98%ED%83%88',
+  },
+  {
+    name: '검사외전',
+    link: 'https://search.naver.com/search.naver?where=nexearch&sm=tab_etc&mra=bkEw&pkid=68&os=2072630&qvt=0&query=%EA%B2%80%EC%82%AC%EC%99%B8%EC%A0%84',
+  },
+];
+
+const movieImages = document.querySelectorAll('.movie-image');
+
+Array.from(movieImages).map((movieImage, index) => {
+  movieImage.addEventListener('click', () => {
+    window.open(MOVIE_INFO[index].link);
+  });
+});
